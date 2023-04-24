2e316260759000bcc323935f17eb425e6e8609ee
epi-xel <e.pioldi@campus.unimib.it>
2023-04-24 17:29:48 +0200

    Add working Jacobi and Gauss-Seidel, format output

--- main.py
+++ main.py
@@ -1,42 +1,24 @@
 import scipy as sp
+from scipy.io import mmread
+import scipy.sparse.linalg
 import numpy as np
 import time as t
 
-# Load the data
-spa1 = sp.mmread('data/spa1.mtx')
-spa2 = sp.mmread('data/spa2.mtx')
-vem1 = sp.mmread('data/vem1.mtx')
-vem2 = sp.mmread('data/vem2.mtx')
+max_iter = 20000
 
-max_iter = 1000
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 # TODO 1: Input management
 # TODO 2: Check if the matrix is diagonally dominant etc
-# TODO 3: Calculate time for each method 
-
-def solve(A, b, tol, update):
-
-    start = t.time()
-    x = init_x(A)
-    k = 0
-    prev_x = x
-    while not converged(x, prev_x, k, A, b, tol):
-        prev_x = x
-        x = update(A, b, x)
-        k += 1
-        if(k > max_iter):
-            print("Max iterations reached")
-            break
-    end = t.time()
-
-    time_elapsed = end - start
-    res = dict();
-    res['solution'] = x
-    res['time'] = time_elapsed
-    res['iterations'] = k
-
-    return res
-
 
 # Initialize the solution vector
 def init_x(A):
@@ -45,12 +27,7 @@ def init_x(A):
 
 # Check if the algorithm has converged
 def converged(x, prev_x, k, A, b, tol):
-    if(k == 0):
-        cond1 = False
-    else:
-        cond1 = np.linalg.norm(x - prev_x) / np.linalg.norm(init_x(A)) < tol 
-    cond2 = np.linalg.norm(A.dot(x) - b) / np.linalg.norm(b) < tol
-    return cond1 or cond2
+    return np.linalg.norm(A.dot(x) - b) / np.linalg.norm(b) < tol
 
 
 # Compute the Jacobi update
@@ -64,26 +41,21 @@ def jacobi(A, b, x):
 def forward_substitution(A, b):
     n = A.shape[0]
     x = np.zeros(n)
+    A = A.tocsc()
+
     for i in range(n):
-        x[i] = (b[i] - A[i, :i].dot(x[:i])) / A[i, i]
+        x[i] = (b[i] - A[i, :i].dot(x[:i])) / float(A[i, i])
+ 
     return x
 
 
 # Compute the Gauss-Seidel update
 def gauss_seidel(A, b, x):
-    P = np.tril(A)
-    y = forward_substitution(P, b)
-    return x - y
-
-
-# Test methods with x = [1, 1, ... 1]
-def test(A, tol):
-    x = np.ones(A.shape[1])
-    b = A.dot(x)
-    print("Jacobi")
-    jacobi_res = solve(A, b, tol, jacobi)
-    print("Gauss-Seidel")
-    gauss_seidel_res = solve(A, b, tol, gauss_seidel)
+    P = sp.sparse.tril(A)
+    r = b - A.dot(x)
+    #y = sp.sparse.linalg.spsolve_triangular(P.tocsr(), r)
+    y = forward_substitution(P, r)
+    return x + y
 
 
 # Compute relative error
@@ -91,9 +63,83 @@ def rel_error(x, x_true):
     return np.linalg.norm(x - x_true) / np.linalg.norm(x_true)
 
 
+def solve(A, b, tol, update):
+
+    start = t.time()
+    x = init_x(A)
+    k = 0
+    prev_x = x
+
+    while not converged(x, prev_x, k, A, b, tol):
+        prev_x = x
+        x = update(A, b, x)
+        k += 1
+        if(k > max_iter):
+            print(bcolors.FAIL
+                + "Max iterations reached"
+                + bcolors.ENDC )
+            break
+
+    end = t.time()
+
+    time_elapsed = end - start
+    res = dict();
+    res['solution'] = x
+    res['time'] = time_elapsed
+    res['iterations'] = k
+
+    return res
+
+
 # Print the stats of the solution
-def print_stats(res):
-    print("Solution: ", res['solution'])
-    print("Time: ", res['time'])
-    print("Iterations: ", res['iterations'])
+def print_stats(res, x_true, method, last = False):
+
+    print(bcolors.BOLD
+          + "Stats for "
+          + bcolors.OKCYAN
+          + method 
+          + bcolors.ENDC
+          + bcolors.BOLD
+          + " method" 
+          + bcolors.ENDC)
+    print(bcolors.OKGREEN 
+          + "> Relative error:  " 
+          + bcolors.ENDC 
+          + str(rel_error(res['solution'], x_true)))
+    print(bcolors.OKGREEN 
+          + "> Elapsed time:    " 
+          + bcolors.ENDC 
+          + str(res['time']) + " sec")
+    print(bcolors.OKGREEN
+          + "> Iterations:      " 
+          + bcolors.ENDC 
+          + str(res['iterations']))
+    if not last:
+        print("------------------------------------------")
+    
+
+# Solve the system with each method
+def solve_with_each_method(A, b, x, tol):
+
+    jacobi_res = solve(A, b, tol, jacobi)
+    print_stats(jacobi_res, x, "Jacobi")
+    gauss_seidel_res = solve(A, b, tol, gauss_seidel)
+    print_stats(gauss_seidel_res, x, "Gauss-Seidel", True)
+
+
+def test():
+    
+    # Load the data
+    data = ['spa1', 'spa2', 'vem1', 'vem2']
+
+    # Test methods with x = [1, 1, ... 1]
+    for m in data:
+        A = mmread('data/' + m + '.mtx')
+        x = np.ones(A.shape[1])
+        b = A.dot(x)
+        print(bcolors.HEADER + "Matrix " + m + bcolors.ENDC)
+        solve_with_each_method(A, b, x, 1e-6)
+        print("==========================================")
+
+test()
 
